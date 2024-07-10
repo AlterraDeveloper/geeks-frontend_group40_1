b2cd441c11e4666e19a34e7703278b1bb6c8d6f8
Alterra$Developer <42990792+AlterraDeveloper@users.noreply.github.com>
2024-07-10 18:50:02 +0600

    add lesson 1

---
+++ lesson_1/app.js
@@ -0,0 +1,24 @@
+/*
+alert("Hello group 40 ++++++++++++");
+
+setInterval(() => {
+  const colors = ["red", "orange", "yellow", "green", "blue", "cyan", "purple"];
+  document.body.style.backgroundColor =
+    colors[Math.round(Math.random() * colors.length)];
+}, 200);
+*/
+
+// comments
+
+// variables - переменные
+
+let studentName = "Evgeniy";
+studentName = "Vadim";
+
+// alert(studentName);
+// document.write(studentName);
+console.log(studentName);
+
+studentName = prompt("What is your name?");
+
+console.log("Welcome " + studentName);
